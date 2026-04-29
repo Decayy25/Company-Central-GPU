5ec6d023228d13c1a2353a445fce69f608d60f6f
Decayy25 <mochrizqihermawan615@gmail.com>
2026-04-29 21:09:51 +0700

    home dummy layout

--- src/components/Atoms/Logo.tsx
+++ src/components/Atoms/Logo.tsx
@@ -11,7 +11,7 @@ export default function Logo (props: LogoProps) {
     const { logo = Icon, className = "w-64 h-64", hex } = props;
     return (
       <Fragment>
-          <a href="https://e-commerce-central-gpu.vercel.app/" className={hex}>
+          <a href="https://e-commerce-central-gpu.vercel.app/" target="_blank" className={hex}>
             <img src={logo} className={`${className} hex-border rounded-full `} alt="" />
           </a>
       </Fragment>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,43 @@
 import { Fragment } from "react/jsx-runtime";
 import Logo from "../components/Atoms/Logo"
+import dummy from "../assets/material/Product Title.png"
 export default function Home () {
     return (
-        <Fragment>
-            <div className="my-20 flex px-10 py-10 rounded-lg">
-                <div className="p-1 rounded-full">
-                    <Logo
-                        className="w-24 h-24 fixed border-none rounded-full m-auto mb-10"
-                        hex="hex-border"
-                    />
-                </div>
+      <Fragment>
+        <div className="my-20 mx-auto flex px-10 py-10 min-h-full justify-between bg-white/5">
+          <div className="p-1 rounded-full">
+            <Logo
+              className="w-30 h-30 fixed border-none rounded-full m-auto mb-10"
+              hex="hex-border"
+            />
+          </div>
+
+          <div className="ml-50">
+            <h1 className="text-4xl font-bold mb-4 text-center">
+              Selamat Datang di Central GPU
+            </h1>
+
+            {/* Center Img */}
+            <div className="mb-10 mt-20 ml-auto flex justify-center w-lg">
+              <img src={dummy} alt="img company" className="rounded-2xl" />
             </div>
-        </Fragment>
-    )
+
+            <div className="w-full mb-10">
+              <p className="text-white text-lg font-semibold mb-6 text-center">
+                Temukan berbagai pilihan GPU terbaik untuk kebutuhan gaming,
+                desain grafis, dan komputasi Anda. Kami menyediakan produk
+                berkualitas dengan harga kompetitif.
+              </p>
+            </div>
+
+            <a
+              href="#"
+              className="inline-block bg-[var(--Icy-Aqua)] text-center text-[var(--Nvidia)] font-semibold px-6 py-3 rounded-lg hover:bg-[var(--Intel)] hover:text-white transition-colors duration-300"
+            >
+              Jelajahi Koleksi Kami
+            </a>
+          </div>
+        </div>
+      </Fragment>
+    );
 }
